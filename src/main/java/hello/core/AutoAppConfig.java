66665579fc1b9f596fc453c77c35e05063c9ee85
KoungQ <guu119@naver.com>
2023-12-13 23:09:23 +0900

    commit

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // @Component 어노테이션이 붙은 모든 클래스 스캔
+        // @Configuration 어노테이션이 붙은 클래스를 제외하고 스캔 -> @Configuration 파일과 충돌 방지
+        // @Configuration은 @Component가 기본적으로 적용돼 있음
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {

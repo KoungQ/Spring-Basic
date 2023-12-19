942ecad207f6f7fd5a384cba67e88a7f19ea246c
KoungQ <guu119@naver.com>
2023-12-19 14:00:25 +0900

    commit

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,20 +1,26 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    // DiscountPolicy 를 @Autowired 가 탐색할 때 타입으로 조회하기 때문에 FixDiscountPolicy 와
+    // RateDiscountPolicy 모두 조회하므로 둘 다 @Component 로 등록할 시에 에러 발생하게 됨 -> NoUniqueBeanDefinitionException 발생
 
-    @Autowired
+    @Autowired    // 생성자가 하나일 땐 @Autowired 안붙여도 가능
     public OrderServiceImpl(MemberRepository memberRepository,
-                            DiscountPolicy discountPolicy) {
+                            @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
